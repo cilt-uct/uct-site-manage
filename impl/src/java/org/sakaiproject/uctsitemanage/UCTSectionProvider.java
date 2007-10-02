60377cb867433b5579f2201676b8735077f4b158
David Horwitz <dhorwitz@gmail.com>
2007-10-02 13:01:12 +0000

    Initial import.

---
+++ impl/src/java/org/sakaiproject/uctsitemanage/UCTSectionProvider.java
@@ -0,0 +1,15 @@
+package org.sakaiproject.uctsitemanage;
+
+import java.util.List;
+
+import org.sakaiproject.sitemanage.api.AffiliatedSectionProvider;
+
+public class UCTSectionProvider implements AffiliatedSectionProvider {
+
+	public List getAffiliatedSectionEids(String userId,
+			String academicSessionEid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
