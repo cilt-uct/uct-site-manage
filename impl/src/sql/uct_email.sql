5ef086ea60fe304240200b141cd50440bf38202b
David Horwitz <dhorwitz@gmail.com>
2007-12-11 15:11:18 +0000

    Updated email templates

--- impl/src/sql/uct_email.sql
+++ impl/src/sql/uct_email.sql
@@ -1,36 +1,3 @@
- create table UCT_EMAIL (
- id serial,
-     templateKey varchar(255),
-     template text,
-     subject text,
-     Index  (templateKey)
-     );
-insert into UCT_EMAIL(templateKey,subject, template) values ('notifyNewUserEmail','$productionSiteName Site Notification',
-'$userName,
-
-You have been added to the $localSakaiName ($localSakaiUrl) by $currentUserName.
-
-Your username is: $userName
-Your password is
-$newPassword
-
-You can later go to the Account tool in your My Workspace site to reset it.');
-
-
-insert into UCT_EMAIL(templateKey,subject,template) values ('notifyAddedParticipant','$productionSiteName New User Notification',
-'Dear $userName,
-
-You have been added to the following $localSakaiName site:
-$siteName
-by $currentUserName.
-
-To log in:
-1. In your internet browser open $localSakaiName: $localSakaiUrl
-2. At the top of the page type your username and password, and then click ''Login''.
-3. You can now access your site either by clicking on the Site Name, which appears as a tab in a row across the upper part of your screen.
-');
-
-
 
 insert into EMAIL_TEMPLATE_ITEM(template_Key,subject,message, last_modified) values ('sitemange.notifyAddedParticipant','$productionSiteName New User Notification',
 'Dear $userName,
@@ -39,20 +6,31 @@ You have been added to the following $localSakaiName site:
 $siteName
 by $currentUserName.
 
-To log in:
-1. In your internet browser open $localSakaiName: $localSakaiUrl
-2. At the top of the page type your username and password, and then click ''Login''.
-3. You can now access your site either by clicking on the Site Name, which appears as a tab in a row across the upper part of your screen.
-', now());
+To go to this site, login to $localSakaiName at $localSakaiUrl with your username ($userName) and password.
+
+You can then access the site by clicking on the site name, which appears as a tab in a row across the top part of the page, or selecting the site from the drop-down list on the right.
+
+If you have any questions about $localSakaiName or how to access this site, please feel free to contact the $localSakaiName helpdesk by replying to this email or emailing help@vula.uct.ac.za.
+
+Online help is also available by clicking on the Help link in any page.
+
+Regards
+The Vula Team, University of Cape Town', now());
 
 
 insert into EMAIL_TEMPLATE_ITEM(template_Key,subject, message, last_modified) values ('sitemanage.notifyNewUserEmail','$productionSiteName Site Notification',
-'$userName,
+'Dear $userName,
 
-You have been added to the $localSakaiName ($localSakaiUrl) by $currentUserName.
+You have been added to $localSakaiName ($localSakaiUrl) by $currentUserName ($currentUserEmail).
 
 Your username is: $userName
-Your password is
-$newPassword
+Your password is: $newPassword
 
-You can later go to the Account tool in your My Workspace site to reset it.', now());
+Once you have logged in, you may change your password by clicking on Account in My Workspace. You may also add your full name and other profile information by clicking on Profile.
+
+If you have any questions about Vula, please feel free to contact the Vula helpdesk by replying to this email or emailing help@vula.uct.ac.za.
+
+Online help is also available by clicking on the Help link on any page.
+
+Regards
+The Vula team, University of Cape Town', now());
