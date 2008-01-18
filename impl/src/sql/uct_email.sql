4f32278ddd9076489fc0c7f20e350d1a131a9596
David Horwitz <dhorwitz@gmail.com>
2008-01-18 09:11:51 +0000

    updated templates

--- impl/src/sql/uct_email.sql
+++ impl/src/sql/uct_email.sql
@@ -1,16 +1,18 @@
 
-insert into EMAIL_TEMPLATE_ITEM(template_Key,subject,message, last_modified) values ('sitemange.notifyAddedParticipant','$productionSiteName New User Notification',
-'Dear $userName,
+#delete from EMAIL_TEMPLATE_ITEM;
 
-You have been added to the following $localSakaiName site:
-$siteName
-by $currentUserName.
+insert into EMAIL_TEMPLATE_ITEM(template_Key,subject,message, last_modified) values ('sitemange.notifyAddedParticipant','${productionSiteName} New User Notification',
+'Dear ${userName},
 
-To go to this site, login to $localSakaiName at $localSakaiUrl with your username ($userName) and password.
+You have been added to the following ${localSakaiName} site:
+${siteName}
+by ${currentUserName}.
+
+To go to this site, login to ${localSakaiName} at $localSakaiUrl with your username (${userName}) and password.
 
 You can then access the site by clicking on the site name, which appears as a tab in a row across the top part of the page, or selecting the site from the drop-down list on the right.
 
-If you have any questions about $localSakaiName or how to access this site, please feel free to contact the $localSakaiName helpdesk by replying to this email or emailing help@vula.uct.ac.za.
+If you have any questions about ${localSakaiName} or how to access this site, please feel free to contact the ${localSakaiName} helpdesk by replying to this email or emailing help@vula.uct.ac.za.
 
 Online help is also available by clicking on the Help link in any page.
 
@@ -18,13 +20,13 @@ Regards
 The Vula Team, University of Cape Town', now());
 
 
-insert into EMAIL_TEMPLATE_ITEM(template_Key,subject, message, last_modified) values ('sitemanage.notifyNewUserEmail','$productionSiteName Site Notification',
-'Dear $userName,
+insert into EMAIL_TEMPLATE_ITEM(template_Key,subject, message, last_modified) values ('sitemanage.notifyNewUserEmail','${productionSiteName} Site Notification',
+'Dear ${userName},
 
-You have been added to $localSakaiName ($localSakaiUrl) by $currentUserName ($currentUserEmail).
+You have been added to ${localSakaiName} (${localSakaiUrl}) by $currentUserName (${currentUserEmail}).
 
-Your username is: $userName
-Your password is: $newPassword
+Your username is: ${userName}
+Your password is: ${newPassword}
 
 Once you have logged in, you may change your password by clicking on Account in My Workspace. You may also add your full name and other profile information by clicking on Profile.
 
